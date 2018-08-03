f2ff8467b0e5f6fd53b70676c07a719d3edecc53
Chris Hatch <hatch@tuta.io>
2018-08-03 16:39:12 +0700

    fix test - replace timestamp with fixed string for snapshot compares

--- webot.test.js
+++ webot.test.js
@@ -5,6 +5,7 @@ const webot = require('./webot')
 const {WX_TOKEN} = require('./config')
 
 const USER = 'odQ_C0gylEtCpMTORAqxQoLd1Zmg'
+const TIMESTAMP = 1533288151
 
 const calculateSignature = (timestamp, nonce) => {
   const s = [WX_TOKEN, timestamp, nonce].sort().join('')
@@ -16,12 +17,11 @@ const calculateSignature = (timestamp, nonce) => {
 
 const signatureQuery = () => {
   const nonce = 1
-  const timestamp = Math.floor(Date.now() / 1000)
   return {
     nonce,
     openid: USER,
-    signature: calculateSignature(timestamp, nonce),
-    timestamp,
+    signature: calculateSignature(TIMESTAMP, nonce),
+    timestamp: TIMESTAMP,
   }
 }
 
@@ -30,7 +30,7 @@ const xmlMsg = msg =>
 <xml>
 <ToUserName><![CDATA[gh_97f347ad4987]]></ToUserName>
 <FromUserName><![CDATA[${USER}]]></FromUserName>
-<CreateTime>1533300000</CreateTime>
+<CreateTime>${TIMESTAMP}</CreateTime>
 <MsgType><![CDATA[text]]></MsgType>
 <Content><![CDATA[${msg}]]></Content>
 <MsgId>6585322584931228410</MsgId>
@@ -45,8 +45,18 @@ const sendMsg = msg =>
     .send(xmlMsg(msg))
     .expect(200)
 
+const stripCreateTime = xml =>
+  xml.replace(
+    /<CreateTime>.*<\/CreateTime>/,
+    '<CreateTime>redacted :)</CreateTime>'
+  )
+
 test('unknown command returns error message', () =>
-  sendMsg('/coffee').then(rsp => expect(rsp.text).toMatchSnapshot()))
+  sendMsg('/coffee').then(rsp =>
+    expect(stripCreateTime(rsp.text)).toMatchSnapshot()
+  ))
 
 test('/help command returns help text', () =>
-  sendMsg('/help').then(rsp => expect(rsp.text).toMatchSnapshot()))
+  sendMsg('/help').then(rsp =>
+    expect(stripCreateTime(rsp.text)).toMatchSnapshot()
+  ))
